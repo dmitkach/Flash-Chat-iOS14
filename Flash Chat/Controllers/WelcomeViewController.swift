f6fd74546c403e1d5c4d09689f40ba6097a9ba5e
Dmitry Tkach <dmitkach@icloud.com>
2021-01-07 19:05:25 +0300

    - Added pod to animate app name on main screen when opened

--- Flash Chat/Controllers/WelcomeViewController.swift
+++ Flash Chat/Controllers/WelcomeViewController.swift
@@ -5,27 +5,17 @@
 //  Created by Dmitry Tkach on 05.01.2021.
 //
 
-import Foundation
+import CLTypingLabel
 import UIKit
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = ""
-        
-        let titleText = "⚡️FlashChat"
-        var charDelay = 1.0
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charDelay, repeats: false) { _ in
-                self.titleLabel.text?.append(letter)
-            }
-            charDelay += 1
-        }
-
+        titleLabel.text = "⚡️FlashChat"
        
     }
     
